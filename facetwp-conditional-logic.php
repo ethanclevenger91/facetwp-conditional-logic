c977e80a5250c4401b38634fa6c57af069e8d828
mgibbs189 <mgibbs189@gmail.com>
2016-06-15 16:10:36 -0400

    Tagged 1.1

--- facetwp-conditional-logic.php
+++ facetwp-conditional-logic.php
@@ -1,29 +1,16 @@
 <?php
 /*
 Plugin Name: FacetWP - Conditional Logic
+Plugin URI: https://facetwp.com/
 Description: Toggle facets based on certain conditions
-Version: 1.0
+Version: 1.1
 Author: FacetWP, LLC
-Author URI: https://facetwp.com/
-GitHub Plugin URI: https://github.com/FacetWP/facetwp-conditional-logic
-
-Copyright 2016 FacetWP, LLC
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, see <http://www.gnu.org/licenses/>.
+GitHub URI: facetwp/facetwp-conditional-logic
 */
 
 defined( 'ABSPATH' ) or exit;
+
+include( dirname( __FILE__ ) . '/github-updater.php' );
 
 class FacetWP_Conditional_Logic_Addon
 {
@@ -35,7 +22,7 @@ class FacetWP_Conditional_Logic_Addon
 
     function __construct() {
 
-        define( 'FWPCL_VERSION', '1.0' );
+        define( 'FWPCL_VERSION', '1.1' );
         define( 'FWPCL_DIR', dirname( __FILE__ ) );
         define( 'FWPCL_URL', plugins_url( '', __FILE__ ) );
         define( 'FWPCL_BASENAME', plugin_basename( __FILE__ ) );
